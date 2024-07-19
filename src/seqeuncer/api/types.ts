f6d929a9d19c215ba165e84fc6b8bd956b9af5ca
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-07-19 14:44:53 +0800

    dev: fmt files

--- src/seqeuncer/api/types.ts
+++ src/seqeuncer/api/types.ts
@@ -1,11 +1,22 @@
 import type { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export interface ISequencerApiClient {
-      get<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
+  get<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>;
 
-       post<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>
+  post<T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<AxiosResponse<T>>;
 
-       put<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>
+  put<T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<AxiosResponse<T>>;
 
-       delete<T>(URL: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>
+  delete<T>(
+    URL: string,
+    config?: AxiosRequestConfig,
+  ): Promise<AxiosResponse<T>>;
 }
